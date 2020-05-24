dc6fd6ad6531eacb5e77f5c1967c1a77de7310eb
AmmarMedAyoub <45518536+medayoubammar@users.noreply.github.com>
2020-05-24 07:55:53 +0200

    Add files via upload

---
+++ CRUDjavaee/src/crud/Student.java
@@ -0,0 +1,49 @@
+package crud;
+
+public class Student{
+
+	private int id;
+	private String name,email,password,country;
+	
+	
+	public Student(String name, String email, String password, String country) {
+	
+		
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.country = country;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+}
